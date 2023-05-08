0b60ac7587e1e84e8f35d4ae1febde76e93e0485
Luis3lvira <94944449+Luis3lvira@users.noreply.github.com>
2023-05-08 12:56:01 -0600

    Add files via upload

---
+++ Filosofo.java
@@ -0,0 +1,91 @@
+package CenaFilosofos;  
+import java.awt.Color;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+/** 
+ * 
+ * @author luise
+ */  
+public class Filosofo implements Runnable{  
+int id,res;  
+Thread t;  
+JButton filosofo;
+JLabel derecho;
+JLabel izquierdo;
+JLabel resultado;
+String proceso;
+JTextArea textArea;
+Filosofo(int id,JLabel izquierdo, JLabel derecho,JButton filosofo,JLabel resultado, JTextArea textArea){  
+    this.id = id;  
+    this.derecho = derecho;  
+    this.izquierdo = izquierdo;  
+    this.filosofo=filosofo;
+    this.resultado=resultado;
+    this.textArea=textArea;
+    t = new Thread(this);
+    t.start();  
+    
+}  
+public void  run(){ 
+    for(int i =0;i<4;i++){  
+        synchronized(this.izquierdo){  
+           synchronized(this.derecho){  
+             comer();     
+            } 
+        }
+        pensar();  
+    } 
+   
+}  
+void comer () {  
+    derecho.setText("Ocupado");
+    derecho.setForeground(Color.red);
+  
+    izquierdo.setText("Ocupado");
+    izquierdo.setForeground(Color.red);
+    
+    filosofo.setText("Comiendo su maruchitan");
+    filosofo.setBackground(Color.GREEN);
+
+    res=Integer.parseInt(resultado.getText());
+    res+=1;
+    resultado.setText(String.valueOf(res));
+    proceso= "Filosofo= "+(id+1)+ " Comiendo usa sus tenedores\n";
+    textArea.append(proceso);
+    try{  
+        Thread.sleep(5000);  
+    }catch(InterruptedException e){  
+    }
+    derecho.setText("Libre");
+    derecho.setForeground(Color.black);
+    
+    izquierdo.setText("Libre");
+    izquierdo.setForeground(Color.black);
+    
+    filosofo.setText("Filosofando");   
+    filosofo.setBackground(Color.DARK_GRAY);
+    proceso="Fil.= "+(id+1)+ " Deja de comer y filosofa, desocupa sus tenedores\n";  
+    textArea.append(proceso);
+}  
+void pensar(){  
+    derecho.setText("Libre");
+    derecho.setForeground(Color.black);
+    
+    izquierdo.setText("Libre");
+    izquierdo.setForeground(Color.black);
+    
+    filosofo.setText("Filosofando");   
+    filosofo.setBackground(Color.DARK_GRAY);
+
+    try{  
+
+        Thread.sleep(5000);  
+    }catch(InterruptedException e){  
+    }          
+}  
+
+}  
